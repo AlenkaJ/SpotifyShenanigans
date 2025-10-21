082bbefbd1e8e9f7c405fb67001722404e86d3bb
AlenkaJ <aja.jarolimova@seznam.cz>
2025-10-21 12:23:26 +0200

    remove django_app (moved to separate repository)

--- django_app/analytics_site/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "analytics_site.settings")
-
-app = Celery("analytics_site")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
--- django_app/spotify_filter/tasks.py
+++
@@ -1,7 +0,0 @@
-from celery import shared_task
-from .spotify_import.import_logic import import_from_spotify
-
-
-@shared_task
-def import_spotify_data_task():
-    import_from_spotify()
